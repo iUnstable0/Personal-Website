e6982e250c3848a30510b23b92bba624bedc4405
iUnstable0 <iunstable0@gmail.com>
2023-04-08 01:06:41 +0100

    Deploy

--- Server/src/graphql/resolver/modules/query/video.ts
+++ Server/src/graphql/resolver/modules/query/video.ts
@@ -1,57 +1,55 @@
 import { GraphQLError } from "graphql";
 
-import lib_redis from "../../../../modules/redis";
-import lib_cache from "../../../../modules/cache";
-import lib_storage from "../../../../modules/storage";
-import lib_error from "../../../../modules/error";
-import { encode } from "punycode";
+import lib_cache from "@iunstable0/server-libs/build/cache";
+import lib_storage from "@iunstable0/server-libs/build/storage";
+import lib_error from "@iunstable0/server-libs/build/error";
 
 export default class query_video {
-	public static async getVideos(args: any) {
-		let videos: any;
+  public static async getVideos(args: any) {
+    let videos: any;
 
-		try {
-			const cachedVideos: any = await lib_cache.get(`cache_videos`);
+    try {
+      const cachedVideos: any = await lib_cache.get(`cache_videos`);
 
-			if (cachedVideos.data) {
-				videos = cachedVideos.data;
-			} else {
-				videos = await lib_storage.listFiles(
-					`videos/753ab64c-202f-4e1f-a01f-2a345d09f17e`
-				);
+      if (cachedVideos.data) {
+        videos = cachedVideos.data;
+      } else {
+        videos = await lib_storage.listFiles(
+          `videos/753ab64c-202f-4e1f-a01f-2a345d09f17e`
+        );
 
-				await lib_cache.set(`cache_videos`, videos, 60);
-			}
-		} catch (error) {
-			const referenceCode = lib_error.generateReferenceCode();
+        await lib_cache.set(`cache_videos`, videos, 60);
+      }
+    } catch (error) {
+      const referenceCode = lib_error.generateReferenceCode();
 
-			console.error(
-				`🚨 [Video Query] [${referenceCode}] Failed to get videos`,
-				error
-			);
+      console.error(
+        `[Video Query] [${referenceCode}] Failed to get videos`,
+        error
+      );
 
-			throw new GraphQLError(`Internal server error. Ref: ${referenceCode}`, {
-				extensions: {
-					code: "INTERNAL_SERVER_ERROR",
-					http: { status: 500 },
-				},
-			});
-		}
+      throw new GraphQLError(`Internal server error. Ref: ${referenceCode}`, {
+        extensions: {
+          code: "INTERNAL_SERVER_ERROR",
+          http: { status: 500 },
+        },
+      });
+    }
 
-		videos.shift();
+    videos.shift();
 
-		// Videos is sth like ["videos/123/name.mp4"]
-		// Name might contain slash so we split by slash and get from index 2 to the end and join them back to get real name
-		// Also remove the video extension (might be mp4 or webm)
-		// video name could contain dot aswell so we split by dot remove last element and join them back
+    // Videos are sth like ["videos/123/name.mp4"]
+    // Name might contain slash, so we split by slash and get from index 2 to the end and join them back to get real name
+    // Also remove the video extension (might be mp4 or webm)
+    // video name could contain dot as well, so we split by dot remove last element and join them back
 
-		return videos.map((video: string) => {
-			const name = video.split(`/`).slice(2).join(`/`);
+    return videos.map((video: string) => {
+      const name = video.split(`/`).slice(2).join(`/`);
 
-			return {
-				title: name.split(`.`).slice(0, -1).join(`.`),
-				path: encodeURI(`https://objects.iunstable0.com/${video}`),
-			};
-		});
-	}
+      return {
+        title: name.split(`.`).slice(0, -1).join(`.`),
+        path: encodeURI(`https://objects.iunstable0.com/${video}`),
+      };
+    });
+  }
 }
