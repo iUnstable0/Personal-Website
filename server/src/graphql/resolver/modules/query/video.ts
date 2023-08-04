a58a2b39a1f6b7e66b4090e4eb50f388ccb3149f
iUnstable0 <iunstable0@gmail.com>
2023-08-05 00:03:59 +0700

    dawd
    awdsz

--- server/src/graphql/resolver/modules/query/video.ts
+++ server/src/graphql/resolver/modules/query/video.ts
@@ -6,26 +6,74 @@ import lib_error from "@iunstable0/server-libs/build/error";
 
 export default class query_video {
   public static async getVideos(args: any) {
+    // console.log("getting vid");
     let videos: any;
 
     try {
-      const cachedVideos: any = await lib_cache.get(`cache_videos`);
+      const videoDirList = (await lib_storage.listFiles("videos")).filter(
+        (filename) => filename.includes("-video"),
+      );
+
+      console.log(videoDirList);
+
+      // console.log("viddor");
+
+      let currentVideoDir = videoDirList[0];
+
+      console.log(currentVideoDir);
+
+      if (videoDirList.length > 1) {
+        // console.log("viddor3");
+        //   Compare the number of children in each directory, and get the one with the most children
+        //   This is to prevent the case where the video directory is empty
+
+        let maxChildren = 0;
+
+        console.log(videoDirList);
+
+        for (const videoDir of videoDirList) {
+          // console.log("viddor3", videoDir);
+          const children = await lib_storage.listFiles(`videos/${videoDir}`);
+
+          // console.log("viddor3", children);
+
+          if (children.length > maxChildren) {
+            // console.log("viddor3oass");
+
+            maxChildren = children.length;
+            currentVideoDir = videoDir;
+          } else if (children.length === maxChildren) {
+            // console.log("viddor3fail");
+            currentVideoDir = videoDir;
+          }
+        }
+
+        // console.log("viddor4");
+      }
+
+      const cachedVideos: any = await lib_cache.get(
+        `cache_videos_${currentVideoDir}`,
+      );
 
       if (cachedVideos.data) {
         videos = cachedVideos.data;
+
+        // console.log("viddor5");
       } else {
-        videos = await lib_storage.listFiles(
-          `videos/753ab64c-202f-4e1f-a01f-2a345d09f17e`
-        );
+        videos = await lib_storage.listFiles(currentVideoDir);
 
         await lib_cache.set(`cache_videos`, videos, 60);
+
+        // console.log("viddor6");
       }
+
+      // console.log("viddor7");
     } catch (error) {
       const referenceCode = lib_error.generateReferenceCode();
 
       console.error(
         `[Video Query] [${referenceCode}] Failed to get videos`,
-        error
+        error,
       );
 
       throw new GraphQLError(`Internal server error. Ref: ${referenceCode}`, {
@@ -36,7 +84,13 @@ export default class query_video {
       });
     }
 
+    // console.log("viddor8");
+
     videos.shift();
+
+    // console.log(videos);
+
+    // console.log("viddor9");
 
     // Videos are sth like ["videos/123/name.mp4"]
     // Name might contain slash, so we split by slash and get from index 2 to the end and join them back to get real name
