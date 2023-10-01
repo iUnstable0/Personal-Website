a81307e3f2c392f402a144ceb45c6280198b5d75
iUnstable0 <iunstable0@gmail.com>
2023-10-01 16:25:34 +0700

    Staging

--- website/components/pages/contact.tsx
+++ website/components/pages/contact.tsx
@@ -1,25 +1,19 @@
 import styles from "styles/Index.module.scss";
 
 export default function Component({
-  setPage,
+	setPage,
 }: {
-  setPage: (page: string) => void;
+	setPage: (page: string) => void;
 }) {
-  return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Contact me</h1>
+	return (
+		<>
+			<div className={styles.container}>
+				<h1 className={styles.title}>Contact me</h1>
 
-        <p className={styles.description}>
-          Hmu on Discord (top right corner) or email me admin@gang.email
-          <br />
-          <br />
-          If you{"'"}re my friend and want an @gang.email address (self-hosted
-          by me)
-          <br />
-          just ask me for it. I{"'"}m more than happy to give it to you.
-        </p>
-      </div>
-    </>
-  );
+				<p className={styles.description}>
+					Hmu on Discord (top right corner) or email me admin@gang.email
+				</p>
+			</div>
+		</>
+	);
 }
