40a5810c55f0b12925b87c90b2b0de8e4ed16b1c
iUnstable0 <iunstable0@pm.me>
2024-03-23 02:19:51 +0700

    update contact and about page

--- website/components/pages/contact.tsx
+++ website/components/pages/contact.tsx
@@ -11,7 +11,8 @@ export default function Component({
 				<h1 className={styles.title}>Contact me</h1>
 
 				<p className={styles.description}>
-					Hmu on Discord (top right corner) or email me contact@iunstable0.com
+					Hmu on Discord (top right corner) or email me at{" "}
+					<a href="mailto:me@iunstable0.com">me@iunstable0.com</a>
 				</p>
 			</div>
 		</>
